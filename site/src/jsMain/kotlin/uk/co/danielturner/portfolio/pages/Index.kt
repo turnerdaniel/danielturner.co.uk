a01ff1000f6bd7168beff7c867d2aec17a159e56
Daniel Turner <35703802+turnerdaniel@users.noreply.github.com>
2025-12-06 22:53:50 +0000

    Refactor to use kobweb

---
+++ site/src/jsMain/kotlin/uk/co/danielturner/portfolio/pages/Index.kt
@@ -0,0 +1,72 @@
+package uk.co.danielturner.portfolio.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.StyleVariable
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.core.data.add
+import com.varabyte.kobweb.core.init.InitRoute
+import com.varabyte.kobweb.core.init.InitRouteContext
+import com.varabyte.kobweb.core.layout.Layout
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.breakpoint.displayIfAtLeast
+import com.varabyte.kobweb.silk.style.toAttrs
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.ColorPalettes
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.fr
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+import uk.co.danielturner.portfolio.HeadlineTextStyle
+import uk.co.danielturner.portfolio.SubheadlineTextStyle
+import uk.co.danielturner.portfolio.components.layouts.PageLayoutData
+import uk.co.danielturner.portfolio.toSitePalette
+
+// Container that has a tagline and grid on desktop, and just the tagline on mobile
+val HeroContainerStyle = CssStyle {
+    base { Modifier.fillMaxWidth().gap(2.cssRem) }
+    Breakpoint.MD { Modifier.margin { top(20.vh) } }
+}
+
+@InitRoute
+fun initHomePage(ctx: InitRouteContext) {
+    ctx.data.add(PageLayoutData("Home"))
+}
+
+@Page
+@Layout(".components.layouts.PageLayout")
+@Composable
+fun HomePage() {
+    Row(HeroContainerStyle.toModifier()) {
+        Box {
+            Column(Modifier.gap(2.cssRem)) {
+                Div(HeadlineTextStyle.toAttrs()) {
+                    SpanText(
+                        "Daniel Turner", Modifier.color(
+                            when (ColorMode.current) {
+                                ColorMode.LIGHT -> Colors.Black
+                                ColorMode.DARK -> Colors.White
+                            }
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
